955b2a4c9ddaef0590442a982eae18de600c0589
Ayşe Beyza Gür <beyyzgur@gmail.com>
2026-05-18 19:36:21 +0300

    feat: add initial project and first updates

---
+++ NotHesaplamaProject/NotHesaplamaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotHesaplamaProject
+//
+//  Created by beyyzgur on 18.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
